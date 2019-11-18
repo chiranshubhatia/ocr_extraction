2648bdbf269eb27aa363384d2d58d2a532bb21fb
chiranshu bhatia <50030365+chiranshubhatia@users.noreply.github.com>
2019-11-18 14:40:15 +0530

    Update application.py

--- application.py
+++ application.py
@@ -23,16 +23,9 @@ app=Flask(__name__)
 @app.route('/',methods=['POST','GET'])
 def extract_text():
     if request.method=='POST':
-        print(request.files['file'])
-        f=request.files['file']
-        print(f.filename)
-        if('.pdf' in f.filename ):
-            
-            f.save(secure_filename(f.filename))
-            png_file=pdf_to_png(f.filename.replace(' ','_'))
-                       
-            image_path=png_file.pdf_to_png()
-            image_data=open(image_path[0],"rb").read()
+    
+        image_data=requests.get('https://imagecleaningpreprocesso.blob.core.windows.net/sendtoaiapiblob/9053507b-f038-4d4f-bbf1-75c6354a4018')
+        if image_data.status_code==200:
             texual_data=hcdf_form_text_extraction.ocr_extraction()
             text=texual_data.text_extract(image_data)
             text_new=hcdf_form_text_extraction.text_cleaning(text)
@@ -46,15 +39,48 @@ def extract_text():
     return '''
 <html><head><title>Upload new File</title>
     </head><body><h1>OCR extraction for HDCF form 1500</h1>
-     <h2>Upload the file in pdf format</h2>
-       
+     <h2>HIT button for demo run</h2>
+  
     <form method="post" enctype="multipart/form-data">
-      <p><input type="file" name="file">
-      
-         <input type="submit" value="Upload">
+          
+         <input type="submit" value="RUN">
          </p>
          </form></h2><div style="background-color: rgb(255, 143, 0); display: none; color: white; text-align: center; position: fixed; top: 0px; left: 0px; width: 100%; height: auto; min-width: 100%; min-height: auto; max-width: 100%; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif; cursor: pointer; padding: 5px;"><span style="color: white; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif;">You have turned off the paragraph player. You can turn it on again from the options page.</span><img src="chrome-extension://gfjopfpjmkcfgjpogepmdjmcnihfpokn/img/icons/icon-close_16.png" style="width: 20px; height: auto; min-width: 20px; min-height: auto; max-width: 20px; float: right; margin-right: 10px;"></div></body></html>
 '''
+# def extract_text():
+#     if request.method=='POST':
+#         print(request.files['file'])
+#         f=request.files['file']
+#         print(f.filename)
+#         if('.pdf' in f.filename ):
+            
+#             f.save(secure_filename(f.filename))
+#             png_file=pdf_to_png(f.filename.replace(' ','_'))
+                       
+#             image_path=png_file.pdf_to_png()
+#             image_data=open(image_path[0],"rb").read()
+#             texual_data=hcdf_form_text_extraction.ocr_extraction()
+#             text=texual_data.text_extract(image_data)
+#             text_new=hcdf_form_text_extraction.text_cleaning(text)
+#             entity_mapping=hcdf_form_text_extraction.entity_extraction()
+#             print("before extraction",entity_mapping.info)
+#             entity_mapping.get_entites(text_new)
+#             print("after extraction",entity_mapping.info)
+#             return pd.DataFrame.from_dict(entity_mapping.info,orient='index').to_html()
+#         else:
+#             return redirect(url_for('error_page'))
+#     return '''
+# <html><head><title>Upload new File</title>
+#     </head><body><h1>OCR extraction for HDCF form 1500</h1>
+#      <h2>Upload the file in pdf format</h2>
+       
+#     <form method="post" enctype="multipart/form-data">
+#       <p><input type="file" name="file">
+      
+#          <input type="submit" value="Upload">
+#          </p>
+#          </form></h2><div style="background-color: rgb(255, 143, 0); display: none; color: white; text-align: center; position: fixed; top: 0px; left: 0px; width: 100%; height: auto; min-width: 100%; min-height: auto; max-width: 100%; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif; cursor: pointer; padding: 5px;"><span style="color: white; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif;">You have turned off the paragraph player. You can turn it on again from the options page.</span><img src="chrome-extension://gfjopfpjmkcfgjpogepmdjmcnihfpokn/img/icons/icon-close_16.png" style="width: 20px; height: auto; min-width: 20px; min-height: auto; max-width: 20px; float: right; margin-right: 10px;"></div></body></html>
+# '''
 
 @app.route('/file_error')        
 def error_page():
